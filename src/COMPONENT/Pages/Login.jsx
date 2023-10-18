5676561d765a99ebf2b38f9eeab79c58c737ecb0
Your NamePritom003 <njahanpritom65@gmail.com>
2023-10-18 14:36:13 +0600

    added style and toggled in login and sign up

--- src/COMPONENT/Pages/Login.jsx
+++ src/COMPONENT/Pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../Layoutss/AuthProvider';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   const{loginuser}=useContext(AuthContext)
@@ -28,10 +29,10 @@ loginuser(email,password)
 
   return (
     <div  className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row-reverse">
+      <div className=" grid gap-10 justify-center align-middle items-center">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+         
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <form onSubmit={handlelogin} className="card-body">
@@ -56,6 +57,11 @@ loginuser(email,password)
             <div className="form-control mt-6">
               <button className="btn btn-primary">Login</button>
             </div>
+            <p> New here ? <Link
+             className='underline-offset-8  text-cyan-600 font-bold' to='/regi'>
+        Register 
+        
+        </Link></p>
           </form>
         </div>
       </div>
--- src/COMPONENT/Pages/Registration.jsx
+++ src/COMPONENT/Pages/Registration.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../Layoutss/AuthProvider';
+import { Link } from 'react-router-dom';
 
 const Registration = () => {
   const{createUser,creategooglesignup}=useContext(AuthContext)
@@ -42,7 +43,7 @@ const handlegooglepop = () => {
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Registration now!</h1>
-          <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <p className="py-6">Welcome to <span className='font-bold' style={{background: 'linear-gradient(135deg, #7B64B6, #C898B9)', WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent'}}>BrandBoulevard</span> , your gateway to a world of fashion and lifestyle possibilities. Registering with us isn't just an option; it's the key to a richer, more fulfilling shopping experience. </p>
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <form onSubmit={handleregister} className="card-body">
@@ -77,14 +78,21 @@ const handlegooglepop = () => {
               </label>
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Registration</button>
+            <button className="btn btn-primary py-2" style={{background: 'linear-gradient(135deg, #7B64B6, #C898B9)'}}>
+        Register
+      </button>
             </div>
+            <p> Already have an account?
+                <Link className="underline-offset-8 ml-4 text-cyan-600 font-bold" to="/login">
+                  Login
+                </Link>
+              </p>
           </form>
         </div>
         <div className="">
           <button
             onClick={handlegooglepop}
-            className="flex shadow-2xl items-center justify-center py-12 md:py-24 px-2 md:px-10 border-cyan-800 border-4 align-middle md:gap-2 text-sm  md:text-xl font-bold"
+            className="flex shadow-2xl items-center justify-center py-12 md:py-24 px-2 md:px-10 border-purple-200 border-4 align-middle md:gap-2 text-sm  md:text-xl font-bold"
           >
             Sign in with Google
             
